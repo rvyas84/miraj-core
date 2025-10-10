f79a809c2cbdea6be47c7cf70dd9801cd2f4493d
rvyas84 <rajanvyas84@gmail.com>
2025-10-10 14:18:13 -0700

    binary searcg

---
+++ basicPrograms/binarySearch.py
@@ -0,0 +1,42 @@
+class binarySearchApp:
+
+    def __init__(self):
+        self.values = []
+    
+    def createList(self, requiredElements):
+
+        for i in range(requiredElements):
+            val = int(input("Enter the value: "))
+            self.values.append(val)
+        
+    def getList(self):
+        return self.values
+    
+    def runBinarySearch(self, searchValue):
+        start = 0
+        end = len(self.values) - 1
+        middle = int((end + start)/2)
+
+        if len(self.values) == 0:
+            print("List is Empty")
+        
+        while start <= end:
+            if searchValue == self.values[middle]:
+                print("Search value {} is found".format(searchValue))
+                break
+            elif searchValue > self.values[middle]:
+                start = middle + 1
+            elif searchValue < self.value[middle]:
+                end = middle -1
+            
+            middle = int((start + end) / 2)
+            
+            if start > end:
+                print("Search value {} is not found".format(searchValue))
+
+            
+
+bs = binarySearchApp()
+bs.createList(5) # 1, 2, 3, 4 ,5
+print(bs.getList())
+bs.runBinarySearch(8)
