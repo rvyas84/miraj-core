dad8af9d81c8bd22d02a8827a8f9ef6ddb63c531
rvyas84 <rajanvyas84@gmail.com>
2025-10-10 18:46:48 -0700

    Linear Search

---
+++ basicPrograms/linearSearch.py
@@ -0,0 +1,28 @@
+class LinerSaerch:
+
+    def __init__(self):
+        self.values = []
+    
+    def createStack(self, totalElements):
+        for i in range(totalElements):
+            val = int(input("Enter value {}: ".format(i+1)))
+            self.values.append(val)
+
+    def searchVal(self, searchTerm):
+        count = 0
+        if len(self.values) == 0:
+            print("List Is Empty")
+        else:
+            for i in range(len(self.values)):
+                count += 1
+                if (searchTerm == self.values[i]):
+                    print ("Search Term {} is found at {}".format(searchTerm, i))
+                    break
+                
+            if count == len(self.values):
+                print("Search Term {} Is Not Found".format(searchTerm))
+
+ls = LinerSaerch()
+ls.createStack(5) # 10, 20, 30, 40, 50
+ls.searchVal(40)
+ls.searchVal(80)
