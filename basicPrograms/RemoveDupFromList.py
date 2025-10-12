92183c8f0584006385c3dc9f289fce4daec0c5bf
rvyas84 <rajanvyas84@gmail.com>
2025-10-12 16:09:41 -0700

    remove dup

---
+++ basicPrograms/RemoveDupFromList.py
@@ -0,0 +1,10 @@
+class RemoveDupFromList:
+
+    def __init__(self):
+        pass
+
+    def removeDupFromList(self, values):
+        print (set(values))
+    
+removeDup = RemoveDupFromList()
+removeDup.removeDupFromList([9,1,3,2,4,6,6,5,3,4,5,7,6,5,3,2,7,8,8,1])
