ef3bef658a978e8e95e30b90d1e71ef0a652f84c
rvyas84 <rajanvyas84@gmail.com>
2025-10-12 20:41:47 -0700

    Valid Parentheses

---
+++ basicPrograms/ValidParentheses.py
@@ -0,0 +1,26 @@
+class CheckParentheses:
+
+    def __init__(self):
+        pass
+
+    def isValidParentheses(self, s: str) -> bool:
+        hashmap = {")":"(", "]":"[", "}":"{"}
+        stk = []
+
+        for c in s:
+            if c not in hashmap:
+                stk.append(c)
+                print(stk)
+            else:
+                if not stk:
+                    return False
+                else:
+                    popped = stk.pop()
+                    print(f"After pop {stk}")
+                    if popped != hashmap[c]:
+                        return False
+        
+        return True
+    
+par = CheckParentheses()
+print(par.isValidParentheses("[[]{{}}]"))
