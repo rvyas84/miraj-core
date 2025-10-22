41bc4dbaa461888315fa0012c4c42d68e3741f46
rvyas84 <rajanvyas84@gmail.com>
2025-10-21 21:53:02 -0700

    design

--- basicPrograms/printingPattern.py
+++ basicPrograms/printingPattern.py
@@ -3,12 +3,22 @@ for i in range(4):
         print("# ",end="")
     print()
 
+# # # # 
+# # # # 
+# # # # 
+# # # # 
+
 print()
 
 for i in range(4):
     for j in range(4-i):
         print("# ",end="")
     print()
+
+# # # # 
+# # # 
+# # 
+# 
 
 print()
 
@@ -17,6 +27,11 @@ for i in range(4):
         print("# ",end="")
     print()
 
+# 
+# # 
+# # # 
+# # # # 
+
 print("Pyramid")
 
 for i in range(1, 5 + 1):
@@ -24,3 +39,8 @@ for i in range(1, 5 + 1):
         print(" " * (5 - i), end="")
         # Print stars
         print("*" * (2 * i - 1))
+
+#     *
+#    ***
+#   *****
+#  *******
