03a103324ac056170fafe225fffd7f5af6c7415d
rvyas84 <rajanvyas84@gmail.com>
2025-11-09 23:33:21 -0800

    find closest

---
+++ basicPrograms/find_closest_number_to_zero.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class FindClosestNumber:
+
+    def findClosestNumber(self, nums: List[int]) -> int:
+
+        closest = nums[0]
+
+        for x in nums:
+            if abs(x) < abs(closest):
+                closest = x
+
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
+        
+
+if __name__ == "__main__":
+    findClosestNum = FindClosestNumber()
+    print(findClosestNum.findClosestNumber([-20,-10,3,4,5,10]))
