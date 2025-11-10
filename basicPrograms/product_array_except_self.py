064fb22b425103576e5ef0f60088f74d3672c796
rvyas84 <rajanvyas84@gmail.com>
2025-11-10 14:15:47 -0800

    Product Array

---
+++ basicPrograms/product_array_except_self.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class ProductArrayExceptSelf:
+
+    def productArray(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
+
+if __name__ == "__main__":
+    productArray = ProductArrayExceptSelf()
+    print(productArray.productArray([2,3,4,5,6]))
+
