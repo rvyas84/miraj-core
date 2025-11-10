c967297712fb6e32f6c4c5af5c1105dc79cf85ff
rvyas84 <rajanvyas84@gmail.com>
2025-11-10 00:45:52 -0800

    find longest sequence

---
+++ basicPrograms/find_longest_sequence_of_number.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class LongestSequence:
+
+    def findLongestSequence(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n-1) not in numSet:
+                length = 0
+                while (n+length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+
+        return longest
+    
+
+if __name__ == "__main__":
+    longestSeq = LongestSequence()
+    print(longestSeq.findLongestSequence([100,4,200,1,3,2,201,202,203,204,205]))
