edaf0e1959fc50ced530fb04cb1524b4e8be8bad
rvyas84 <rajanvyas84@gmail.com>
2025-11-15 16:31:56 -0800

    longest common prefix

---
+++ basicProgramsPython/longest_common_prefix.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class LongestCommonPrefix:
+    def longestCommonPrefix(self, input: List[str]) -> str:
+
+        l = len(input)
+        min_length = float('inf')
+
+        for s in input:
+            if len(s) < min_length:
+                min_length = len(s)
+
+        i = 0
+        while i < min_length:
+            for s in input:
+                if s[i] != input[0][i]:
+                    return s[:i]
+            i += 1
+        
+        return s[:i]
+    
+if __name__ == "__main__":
+    longPreFix = LongestCommonPrefix()
+    print(longPreFix.longestCommonPrefix(["Flower", "Flow", "Flows"]))
+
+# Output: Flow
