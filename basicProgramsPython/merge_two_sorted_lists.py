60a32252e3ad05ff9d2de8610627ba21035b2a50
rvyas84 <rajanvyas84@gmail.com>
2025-11-15 17:18:48 -0800

    merge two sorted arrays

---
+++ basicProgramsPython/merge_two_sorted_lists.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MergeTwoSortedLists:
+    def mergeSortedLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        cur = result
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                cur = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                cur = list2
+                list2 = list2.next
+        
+        cur.next = list1 if list1 else list2
+
+        return result.next
+    
