e717df7d5426dcc48d7e44f94003857ba425e388
rvyas84 <rajanvyas84@gmail.com>
2025-11-16 21:05:02 -0800

    preview

--- basicProgramsPython/reorder_log_files.py
+++ basicProgramsPython/reorder_log_files.py
@@ -16,3 +16,5 @@ class ReorderLogFiles:
 if __name__=="__main__":
     reorder = ReorderLogFiles()
     print(reorder.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
+
+    # Output: ['let1 art can', 'let3 art zero', 'let2 own kit dig', 'dig1 8 1 5 1', 'dig2 3 6']
