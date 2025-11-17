ab905f0a8c2137f9f2b0f4e1a96c5e06c5bd79c4
rvyas84 <rajanvyas84@gmail.com>
2025-11-16 21:02:13 -0800

    reorder log files

---
+++ basicProgramsPython/reorder_log_files.py
@@ -0,0 +1,18 @@
+class ReorderLogFiles:
+    def reorderLogFiles(self, logs):
+        letter_logs = []
+        digit_logs = []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digit_logs.append(log)
+            else:
+                letter_logs.append(log)
+        
+        letter_logs.sort(key=lambda x: (x.split(' ',1)[1], x.split()[0]))
+
+        return letter_logs + digit_logs
+    
+if __name__=="__main__":
+    reorder = ReorderLogFiles()
+    print(reorder.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
