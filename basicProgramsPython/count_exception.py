d7d4a03d5cfb77c9cabcb98b18993b74e2104e3b
rvyas84 <rajanvyas84@gmail.com>
2025-11-17 12:21:27 -0800

    nit

--- basicProgramsPython/count_exception.py
+++ basicProgramsPython/count_exception.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     print(count.countExc('2024-03-12 ERROR NullPointerException at line 52 2024-03-12 ERROR IOException connection reset 2024-03-12 ERROR NullPointerException at line 612024-03-12 ERROR TimeoutException request timed out'))
 
 
-# output: {'NullPointerException': 2, 'IOException': 1, 'TimeoutException': 1}
+# Output: {'NullPointerException': 2, 'IOException': 1, 'TimeoutException': 1}
