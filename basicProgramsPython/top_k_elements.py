4ec5cf73d72df6ed5567d7042cc22ddb1d66155d
rvyas84 <rajanvyas84@gmail.com>
2025-11-18 15:04:58 -0800

    Top K elements of numbers and string

---
+++ basicProgramsPython/top_k_elements.py
@@ -0,0 +1,20 @@
+import collections
+from typing import List
+
+class TopKElements:
+    def topKElemNum(self, input: List[int], k: int):
+        result_dic = collections.Counter(input)
+        result_dic = sorted(result_dic, key = lambda x:result_dic[x], reverse = True)
+
+        return result_dic[:k]
+    
+    def topKElemChar(self, input: str, k: int):
+        result_dic = collections.Counter(input)
+        result_dic = sorted(result_dic, key = lambda x:result_dic[x], reverse = True) # type: ignore
+
+        return result_dic[:k]
+    
+if __name__ == "__main__":
+    topK = TopKElements()
+    print(topK.topKElemNum([1,1,1,1,1,2,2,2,3,3,4,5,5,5,5,5,5,5], 3)) # Output: [5, 1, 2]
+    print(topK.topKElemChar("iiisssttm", 2)) # Output: ['i', 's']
